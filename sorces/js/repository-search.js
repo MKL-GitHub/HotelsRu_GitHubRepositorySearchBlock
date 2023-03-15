fc1b9f7578a7508491d8f06d6fa1d8e8ad020c91
Mikhail Tkachev <tritium.mkl@gmail.com>
2023-03-15 15:23:51 +0600

    Add functions for adding more repositories.

--- sorces/js/repository-search.js
+++ sorces/js/repository-search.js
@@ -1,14 +1,25 @@
 class RepositorySearch {
     GITHUB_URL = "https://api.github.com/search/repositories";
 
+    _requestIsDone = true;
+    _requestStatus;
+    _repositoryPage = 1;
     _searchForm;
     _searchString;
     _repositoryList;
+    _moreRepositories;
+    _moreRepositoriesButton;
+    _moreRepositoriesQuantity;
+    _moreRepositoriesTotal;
 
     constructor() {
         this._searchForm = document.forms.repositorySearchForm;
         this._searchString = this._searchForm.elements.searchString;
         this._repositoryList = document.querySelector(".repository-list");
+        this._moreRepositories = document.querySelector(".github-search-block__more-repositories");
+        this._moreRepositoriesButton = this._moreRepositories.querySelector("button");
+        this._moreRepositoriesQuantity = this._moreRepositories.querySelector("span:nth-child(1)");
+        this._moreRepositoriesTotal = this._moreRepositories.querySelector("span:nth-child(2)");
 
         this.setListeners();
     }
@@ -45,23 +56,38 @@ class RepositorySearch {
         const githubUrl = new URL(this.GITHUB_URL);
         
         githubUrl.searchParams.set("q", this._searchString.value);
+        githubUrl.searchParams.set("page", this._repositoryPage);
         githubUrl.searchParams.set("per_page", REPOSITORY_QUANTITY);
 
         return fetch(githubUrl)
-            .then(response => response.json())
-            .then(data => data.items)
+            .then(response => {
+                this._requestStatus = response.status;
+                return response.json()
+            })
+            .then(data => [data.total_count, data.items])
             .catch(error => console.error(error));
     }
 
-    async createRepositoriesList(repositories) {
-        const items = await repositories;
+    createRepositoriesList(repositories) {
+        const [total_count, items] = repositories;
 
-        this._repositoryList.innerHTML = "";
+        switch (this._requestStatus) {
+            case 200:
+                this._moreRepositories.setAttribute("data-status", 200);
+                break;
+            case 403:
+                this._requestIsDone = true;
+                this._moreRepositories.setAttribute("data-status", 403);
+                break;
+        }
 
-        if (!items || !items.length) {
+        if (this._requestStatus == 200 && (!items || !items.length)) {
             this._repositoryList.setAttribute("data-is-nothing-found", true);
+            this._moreRepositories.style.display = "none";
         }
         else {
+            this._moreRepositories.style.display = "flex";
+
             for (let item of items) {
                 const listItem = document.createElement("li");
                 
@@ -76,28 +102,61 @@ class RepositorySearch {
                 this._repositoryList.append(listItem);
             }
         }
+
+        this._moreRepositoriesQuantity.textContent = this._repositoryList.children.length;
+        this._moreRepositoriesTotal.textContent = total_count;
+
+        if (this._moreRepositoriesQuantity.textContent === this._moreRepositoriesTotal.textContent) {
+            this._moreRepositoriesButton.style.display = "none";
+        }
+        
+        this._requestIsDone = true;
     }
 
-    handleSubmit(event) {
+    async handleSubmit(event, isAdding) {
         event.preventDefault();
 
-        if (!this.isValid()) return;
+        if (!this.isValid() || !this._requestIsDone) return;
 
-        let repositories = this.getRepositories();
+        this._requestIsDone = false;
+
+        if (isAdding) {
+            this._repositoryPage++;
+        }
+        else {
+            this._moreRepositoriesButton.style.display = "block";
+            this._moreRepositories.style.display = "none";
+            this._repositoryList.innerHTML = "";
+            this._repositoryPage = 1;
+        }
+
+        const repositories = await this.getRepositories();
         this._repositoryList.setAttribute("data-is-nothing-found", false);
         this.createRepositoriesList(repositories);
     }
 
-    handleEnterKeyup(event) {
-        if (event.key != "Enter") return;
+    handleMoreRepositoriesMouseup(event) {
+        this.handleSubmit(event, true);
+    }
 
-        this.handleSubmit(event);
+    handleFormEnterKeydown(event) {
+        if (event.keyCode != 13) return;
+        
+        event.preventDefault();
+    }
+
+    handleFormEnterKeyup(event) {
+        if (event.keyCode != 13) return;
+            
+        this.handleSubmit(event, false);
     }
 
     setListeners() {
         this._searchString.addEventListener("input", this.resetSearchAttributes.bind(this));
         this._searchForm.addEventListener("submit", this.handleSubmit.bind(this));
-        document.addEventListener("keyup", this.handleEnterKeyup.bind(this));
+        this._searchForm.addEventListener("keydown", this.handleFormEnterKeydown.bind(this));
+        this._searchForm.addEventListener("keyup", this.handleFormEnterKeyup.bind(this));
+        this._moreRepositoriesButton.addEventListener("mouseup", this.handleMoreRepositoriesMouseup.bind(this));
     }
 }
 
